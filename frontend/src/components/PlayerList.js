f83b38de582cd2e8602f56648651c6667bcd1b82
Josiah <jojotenia@gmail.com>
2025-02-22 11:07:14 -0400

    updated  frontend component player list

--- frontend/src/components/PlayerList.js
+++ frontend/src/components/PlayerList.js
@@ -1,1 +1,27 @@
-hey
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const PlayerList = () => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/players')
+      .then(response => setPlayers(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Football Players</h1>
+      <ul>
+        {players.map(player => (
+          <li key={player._id}>
+            <a href={`/players/${player._id}`}>{player.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PlayerList;
